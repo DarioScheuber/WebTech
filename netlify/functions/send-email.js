9585d94d783a1eb645ef3c26f2ab59d7b3b347df
Dario Scheuber <cheater99@hotmail.ch>
2024-05-12 22:38:49 +0200

    funktions änderungen

--- netlify/functions/send-email.js
+++ netlify/functions/send-email.js
@@ -1,38 +1,36 @@
-
-//netlify send mail 
 const process = require('process');
 const sendgrid = require('@sendgrid/mail');
 
 sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 
 exports.handler = async (event) => {
-  const { email, name, message } = JSON.parse(event.body);
+    try {
+        const { email, name, message } = JSON.parse(event.body);
+        const msgToUser = {
+            to: email,
+            from: 'info@yourdomain.com',
+            subject: 'Bestätigung Ihrer Nachricht',
+            text: `Hallo ${name},\n\nWir haben Ihre Nachricht erhalten: ${message}\n\nVielen Dank!`
+        };
 
-  const msgToUser = {
-    to: email,
-    from: 'info@yourdomain.com', // Ihre E-Mail-Adresse
-    subject: 'Bestätigung Ihrer Nachricht',
-    text: `Hallo ${name},\n\nWir haben Ihre Nachricht erhalten: ${message}\n\nVielen Dank!`
-  };
+        const msgToAdmin = {
+            to: 'fixedemail@yourdomain.com',
+            from: 'info@yourdomain.com',
+            subject: 'Neue Nachricht erhalten',
+            text: `Sie haben eine neue Nachricht von ${name} erhalten:\n\n${message}`
+        };
 
-  const msgToAdmin = {
-    to: 'cheater99@hotmail.ch', // E-Mail des Administrators
-    from: 'info@yourdomain.com',
-    subject: 'Neue Nachricht erhalten',
-    text: `Sie haben eine neue Nachricht von ${name} erhalten:\n\n${message}`
-  };
-
-  try {
-    await sendgrid.send(msgToUser);
-    await sendgrid.send(msgToAdmin);
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: "Emails successfully sent" })
-    };
-  } catch (error) {
-    return {
-      statusCode: error.code,
-      body: JSON.stringify({ message: error.message })
-    };
-  }
+        await sendgrid.send(msgToUser);
+        await sendgrid.send(msgToAdmin);
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: "Emails successfully sent" })
+        };
+    } catch (error) {
+        console.error('Error parsing JSON:', error);
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ message: 'Invalid JSON input' })
+        };
+    }
 };
